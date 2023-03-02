2d62442b4e417096a94f99afa78091ff28901fd0
Noelia Iglesias Cuesta <98741282+noeliaigc@users.noreply.github.com>
2023-03-02 10:14:04 +0100

    Code fixes for exercise 1 and start doing tests for SearchController

--- src/test/java/co/empathy/academy/search/SearchApplicationTests.java
+++ src/test/java/co/empathy/academy/search/SearchApplicationTests.java
@@ -1,11 +1,7 @@
 package co.empathy.academy.search;
 
-import co.empathy.academy.search.controller.SearchController;
-import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
--- src/test/java/co/empathy/academy/search/controller/SearchControllerTests.java
+++ src/test/java/co/empathy/academy/search/controller/SearchControllerTests.java
@@ -2,18 +2,13 @@ package co.empathy.academy.search.controller;
 
 import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.BDDMockito.given;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 @AutoConfigureMockMvc
